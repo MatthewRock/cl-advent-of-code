659990af269200e070e81ca4ee185013667447c9
MatthewRock <maliszmat@gmail.com>
2017-12-03 02:49:22 +0100

    Solution for the day 1 challenge

--- src/inverse-captcha.lisp
+++ src/inverse-captcha.lisp
@@ -1,8 +1,24 @@
 (in-package :advent)
 (annot:enable-annot-syntax)
 
+(defun next-number (number)
+  (floor number 10))
+
+(defun last-digit (number)
+  (mod number 10))
+
 @export
 (defun calculate-sum (number)
   (let ((number-as-string (format nil "~D" number))
         (sum 0))
-    sum))
+    (loop for num = number then next-num
+          for next-num = (next-number num)
+          for current = (last-digit num)
+          for next = (last-digit next-num)
+          until (zerop next)
+          when (= current next) sum current into result
+            ;; Take wrapping into account
+            finally (return (+ result
+                               (if (= current (last-digit number))
+                                   current
+                                   0))))))
